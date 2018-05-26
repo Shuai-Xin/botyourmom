b5c3d9f9f6a14de931bc8889d089e42532a8ae83
Shuai-Xin <xinver22@gmail.com>
2018-05-26 11:50:47 +0800

    fifuck

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ var bot = linebot({
   channelSecret:'aea9c4bea0639a96ceb76d62302a285b',
   channelAccessToken:'elm+hhQtgPLiL+48zVsMJ1Y9Loz9TW4IbnUMsTOGDrGsA3R5mAd77eOEnsLgfDnQ8Klz2Ka2fRVanEA0AwrdklZYjecIue19KuBZoxYFh43cwJ5ZYAIBrETmpiUkPsPXrhsbaby9oYiLEpa3pQb++QdB04t89/1O/w1cDnyilFU='
 });
+
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
