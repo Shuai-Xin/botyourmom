4096208da8bfe8dd79fc99e3d401d103d5740b1a
Shuai-Xin <xinver22@gmail.com>
2018-05-26 11:45:44 +0800

    finalllll

---
+++ index.js
@@ -0,0 +1,8 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId:'1582996232',
+  channelSecret:'aea9c4bea0639a96ceb76d62302a285b',
+  channelAccessToken:'elm+hhQtgPLiL+48zVsMJ1Y9Loz9TW4IbnUMsTOGDrGsA3R5mAd77eOEnsLgfDnQ8Klz2Ka2fRVanEA0AwrdklZYjecIue19KuBZoxYFh43cwJ5ZYAIBrETmpiUkPsPXrhsbaby9oYiLEpa3pQb++QdB04t89/1O/w1cDnyilFU='
+});
